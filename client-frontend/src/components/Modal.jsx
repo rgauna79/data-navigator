652523f1f418eedc35cdc851dbb3734034a783c9
RobertG <49545278+rgauna79@users.noreply.github.com>
2024-02-14 06:54:37 +0000

    working on generating reports

--- client-frontend/src/components/Modal.jsx
+++ client-frontend/src/components/Modal.jsx
@@ -3,23 +3,48 @@ import { Link, useNavigate } from "react-router-dom";
 
 function Modal({ handleClose, columns, data }) {
   const [selectedColumns, setSelectedColumns] = useState([]);
+  const [selectedOptions, setSelectedOptions] = useState({});
+  const [typeReport, setTypeReport] = useState("");
+
   const navigate = useNavigate();
 
   const handleCheckboxChange = (e) => {
-    const { value, checked } = e.target;
+    console.log(selectedOptions);
+    const { name, checked } = e.target;
     if (checked) {
-      setSelectedColumns([...selectedColumns, value]);
+      setSelectedColumns([...selectedColumns, name]);
     } else {
-      setSelectedColumns(selectedColumns.filter((col) => col !== value));
+      setSelectedColumns(selectedColumns.filter((col) => col !== name));
+      setSelectedOptions({ ...selectedOptions, [name]: null });
     }
+  };
+
+  const handleSelectChange = (e) => {
+    const { name, value } = e.target;
+    setSelectedOptions({ ...selectedOptions, [name]: value });
+  };
+
+  const getUniqueValues = (columnName) => {
+    const values = data.map((item) => item[columnName]);
+    return [...new Set(values)];
   };
 
   const handleConfirm = () => {
     if (selectedColumns.length > 0) {
-      // Si hay columnas seleccionadas, se redirige a la página de gráficos
-      navigate("/charts", { state: { selectedColumns, data } });
+      // if there are selected columns and options are selected navigate to charts
+      navigate("/charts", {
+        state: { type: "statistics", selectedColumns, selectedOptions, data },
+      });
+    } else if (typeReport === "mostRepeated") {
+      navigate("/charts", {
+        state: { type: "mostRepeated", selectedOptions, data },
+      });
     }
     handleClose();
+  };
+
+  const handleTypeChange = (e) => {
+    setTypeReport(e.target.value);
   };
 
   return (
@@ -31,22 +56,86 @@ function Modal({ handleClose, columns, data }) {
         >
           &times;
         </span>
-        <h2 className="text-lg font-bold mb-4">Select columns to include:</h2>
-        <div className="flex flex-col">
-          {columns.map((column) => (
-            <div key={column.accessor} className="flex items-center mb-2">
-              <input
-                type="checkbox"
-                id={column.accessor}
-                value={column.accessor}
-                onChange={handleCheckboxChange}
-                className="mr-2"
-              />
-              <label htmlFor={column.accessor} className="text-sm text-black">
-                {column.Header}
-              </label>
+        <h2 className="text-lg font-bold mb-2">Select Type of Report:</h2>
+        <div className="flex flex-col gap-2">
+          <div className="flex items-center mb-2 p-2 bg-gray-200">
+            <input
+              type="radio"
+              name="typeReport"
+              value="allColumns"
+              onChange={handleTypeChange}
+              className="ml-2"
+            />
+            <label htmlFor="allColumns" className="text-sm text-black p-2">
+              All columns
+            </label>
+            <input
+              type="radio"
+              name="typeReport"
+              value="mostRepeated"
+              onChange={handleTypeChange}
+              className="ml-2 "
+            />
+            <label htmlFor="mostRepeated" className="text-sm text-black  p-2">
+              Most repeated values
+            </label>
+          </div>
+          {typeReport === "allColumns" && (
+            <div>
+              {columns.map((column) => (
+                <div key={column.accessor} className="flex items-center mb-2">
+                  <input
+                    type="checkbox"
+                    id={column.accessor}
+                    value={column.accessor}
+                    onChange={handleCheckboxChange}
+                    className="mr-2"
+                    name={column.Header}
+                  />
+                  <label
+                    htmlFor={column.accessor}
+                    className="text-sm text-black"
+                  >
+                    {column.Header}
+                  </label>
+                  {selectedColumns.includes(column.Header) && (
+                    <select
+                      name={column.Header}
+                      value={selectedOptions[column.Header] || ""}
+                      onChange={handleSelectChange}
+                      className="ml-2"
+                    >
+                      <option value="">Select a record</option>
+                      {getUniqueValues(column.accessor).map((value) => (
+                        <option key={value} value={value}>
+                          {value}
+                        </option>
+                      ))}
+                    </select>
+                  )}
+                </div>
+              ))}
             </div>
-          ))}
+          )}
+          {typeReport === "mostRepeated" && (
+            <div>
+              <label htmlFor="mostRepeated" className="text-sm text-black">
+                Select a column
+              </label>
+              <select
+                name="mostRepeated"
+                value={selectedOptions.mostRepeated || ""}
+                onChange={handleSelectChange}
+              >
+                <option value="">Select</option>
+                {columns.map((column) => (
+                  <option key={column.accessor} value={column.accessor}>
+                    {column.Header}
+                  </option>
+                ))}
+              </select>
+            </div>
+          )}
         </div>
         <button
           onClick={handleConfirm}
--- client-frontend/src/pages/ChartPage.jsx
+++ client-frontend/src/pages/ChartPage.jsx
@@ -1,70 +1,114 @@
-import React, { useEffect, useState } from "react";
-import { useLocation, useNavigate, Link } from "react-router-dom";
-import Chart from "chart.js/auto";
+import React, { useState, useEffect } from "react";
+import { useLocation, Link } from "react-router-dom";
 
 function ChartPage() {
   const location = useLocation();
-  const { selectedColumns, data } = location.state || {};
-  const [chartData, setChartData] = useState([]);
-  const navigate = useNavigate();
+  const {
+    type,
+    selectedColumns: initialSelectedColumns,
+    selectedOptions: initialSelectedOptions,
+    data: initialData,
+  } = location.state || {};
+
+  const [selectedColumns, setSelectedColumns] = useState(
+    initialSelectedColumns || [],
+  );
+  const [selectedOptions, setSelectedOptions] = useState(
+    initialSelectedOptions || {},
+  );
+  const [data, setData] = useState(initialData || []);
 
   useEffect(() => {
-    if (!selectedColumns || !data) {
-      navigate("/");
-    }
-  }, [selectedColumns, data, navigate]);
-
-  useEffect(() => {
-    if (selectedColumns && data) {
-      // Lógica para generar los datos del gráfico
-      const newData = selectedColumns.map((column) => ({
-        label: column,
-        value: Math.floor(Math.random() * 100), // Simulando datos aleatorios
-      }));
-      setChartData(newData);
-    }
-  }, [selectedColumns, data]);
-
-  useEffect(() => {
-    if (chartData.length > 0) {
-      // Lógica para crear y actualizar el gráfico
-      const ctx = document.getElementById("myChart").getContext("2d");
-
-      new Chart(ctx, {
-        type: "bar",
-        data: {
-          labels: chartData.map((data) => data.label),
-          datasets: [
-            {
-              label: "Data",
-              data: chartData.map((data) => data.value),
-              backgroundColor: "rgba(255, 99, 132, 0.2)",
-              borderColor: "rgba(255, 99, 132, 1)",
-              borderWidth: 1,
-            },
-          ],
-        },
-        options: {
-          scales: {
-            y: {
-              beginAtZero: true,
-            },
-          },
-        },
+    if (type === "statistics") {
+      const filteredData = data.filter((row) => {
+        return Object.entries(selectedOptions).every(([key, value]) => {
+          return row.includes(value);
+        });
       });
+      setData(filteredData);
     }
-  }, [chartData]);
+  }, []);
+
+  const generateReportData = () => {
+    if (type === "statistics") {
+      const filteredData = data.filter((row) => {
+        return Object.entries(selectedOptions).every(([key, value]) => {
+          return row.includes(value);
+        });
+      });
+      const totalRows = filteredData.length;
+
+      const includedOptions = Object.entries(selectedOptions)
+        .filter(([key, value]) => value !== "")
+        .map(([key, value]) => `${key}: ${value}`);
+
+      return (
+        <div>
+          <p className="mt-4">Selected Options:</p>
+          <ul className="list-disc list-inside">
+            {includedOptions.map((option) => (
+              <li
+                key={option}
+                className="bg-blue-200 text-blue-800 rounded-full px-2 py-1 mr-2 mt-2"
+              >
+                {option}
+              </li>
+            ))}
+          </ul>
+          <p className="mt-4">
+            Total rows for meeting this criteria:
+            <span className="font-bold"> {totalRows}</span>
+          </p>
+        </div>
+      );
+
+      // `Total rows for selected options: ${totalRows}. \nOptions included: ${includedOptions.join("\n")}`;
+    } else if (type === "mostRepeated") {
+      const selectedColumn = selectedOptions.mostRepeated;
+      const optionsCount = {};
+
+      data.forEach((row) => {
+        const option = row[selectedColumn];
+        if (option !== null && option !== "") {
+          optionsCount[option] = (optionsCount[option] || 0) + 1;
+        }
+      });
+
+      const mostRepeatedOptions = Object.entries(optionsCount)
+        .sort((a, b) => b[1] - a[1])
+        .slice(0, 5)
+        .map(([option, count]) => `${option}: ${count}`);
+
+      return mostRepeatedOptions.length > 0
+        ? mostRepeatedOptions.join(", ")
+        : "No data to display";
+    }
+  };
 
   return (
-    <div>
-      <h1>Gráficos</h1>
+    <div className="flex-1 flex flex-col justify-center items-center">
+      <div className="bg-white rounded shadow-lg text-black">
+        <div className="px-4 py-4 bg-blue-500 text-white border border-blue-500 ">
+          <h1 className="text-2xl font-bold ">Reports</h1>
+        </div>
+        {type && (
+          <div className="mt-4 flex flex-col px-8 pt-4 pb-8">
+            <h2 className="text-lg font-bold">
+              {type === "statistics"
+                ? "Personalized options"
+                : "Most repeated values"}{" "}
+            </h2>
+            <p>{generateReportData()}</p>
+          </div>
+        )}
+      </div>
       <Link
         to="/filereader"
-        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-2"
+        state={{ selectedColumns, selectedOptions, data }}
       >
         Back
       </Link>
-      <canvas id="myChart" width="400" height="400"></canvas>
     </div>
   );
 }
