0ab6fa0a433b564112e0e4d106f0b261e2c0473c
RobertG <49545278+rgauna79@users.noreply.github.com>
2024-03-15 05:55:20 +0000

    feat: add hamburguer menu

--- client-frontend/src/components/NavigationBar.jsx
+++ client-frontend/src/components/NavigationBar.jsx
@@ -3,20 +3,31 @@ import { Link, useNavigate } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars, faSignOutAlt } from "@fortawesome/free-solid-svg-icons";
+import logo from "../assets/images/logo.png";
 
 function NavigationBar() {
   const { isLoggedIn, user, logout } = useAuth();
   const navigate = useNavigate();
   const [showMenu, setShowMenu] = useState(false);
   const menuRef = useRef(null);
+  const hamburgerRef = useRef(null);
 
   const handleLogout = () => {
     logout();
     navigate("/login");
   };
 
+  const handleItemClick = () => {
+    setShowMenu(false);
+  };
+
   const handleClickOutside = (event) => {
-    if (menuRef.current && !menuRef.current.contains(event.target)) {
+    if (
+      menuRef.current &&
+      !menuRef.current.contains(event.target) &&
+      hamburgerRef.current &&
+      !hamburgerRef.current.contains(event.target)
+    ) {
       setShowMenu(false);
     }
   };
@@ -28,67 +39,162 @@ function NavigationBar() {
     };
   }, []);
 
-  return (
-    <nav className="bg-gray-800">
-      <div className="flex justify-between items-center px-4 py-2">
-        <ul className="flex items-center">
-          <li>
-            <Link to="/" className="text-white hover:text-gray-300 mr-4">
-              Home
-            </Link>
-          </li>
-          <li>
-            <Link
-              to="/filereader"
-              className="text-white hover:text-gray-300 mr-4"
-            >
-              FileReader
-            </Link>
-          </li>
-          {isLoggedIn && (
-            <li>
-              <Link
-                to="/savedfiles"
-                className="text-white hover:text-gray-300 mr-4"
-              >
-                Saved Files
-              </Link>
-            </li>
-          )}
-        </ul>
+  useEffect(() => {
+    const handleEscapePress = (event) => {
+      if (event.key === "Escape") {
+        setShowMenu(false);
+      }
+    };
 
-        <ul className="flex flex-col items-center">
-          <li>
-            {isLoggedIn ? (
-              <>
-                <Link
-                  to="/profile"
-                  className="text-white hover:text-gray-300 mr-4"
-                >
-                  Welcome {user.username}
+    document.addEventListener("keydown", handleEscapePress);
+
+    return () => {
+      document.removeEventListener("keydown", handleEscapePress);
+    };
+  }, []);
+
+  return (
+    <nav className="bg-gray-800 px-4 sm:px-2 lg:px-4 sticky top-0">
+      <div className=" ">
+        <div className="flex justify-between h-16 items-center">
+          <div className="flex items-center">
+            <div className="flex-shrink-0 flex items-center ">
+              <img className="h-16 rounded py-1" src={logo} alt="Logo" />
+            </div>
+            <div className="hidden md:block ml-4 text-right">
+              <div className="flex space-x-4">
+                <Link to="/" className="text-white hover:text-gray-300">
+                  Home
                 </Link>
-                <button
-                  onClick={handleLogout}
+                <Link
+                  to="/filereader"
                   className="text-white hover:text-gray-300"
+                  onClick={handleItemClick}
                 >
-                  <FontAwesomeIcon icon={faSignOutAlt} />
-                </button>
-              </>
-            ) : (
-              <>
-                <Link
-                  to="/login"
-                  className="text-white hover:text-gray-300 mr-4"
-                >
-                  Login
+                  FileReader
                 </Link>
-                <Link to="/register" className="text-white hover:text-gray-300">
-                  Register
-                </Link>
-              </>
-            )}
-          </li>
-        </ul>
+                {isLoggedIn && (
+                  <Link
+                    to="/savedfiles"
+                    className="text-white hover:text-gray-300"
+                    onClick={handleItemClick}
+                  >
+                    Saved Files
+                  </Link>
+                )}
+              </div>
+            </div>
+          </div>
+          <div className="hidden md:block">
+            <div className="ml-4 flex items-center md:ml-6">
+              {isLoggedIn ? (
+                <>
+                  <span className="text-white">Welcome {user.username}</span>
+                  <button
+                    onClick={handleLogout}
+                    className="ml-4 text-white hover:text-gray-300"
+                  >
+                    <FontAwesomeIcon icon={faSignOutAlt} />
+                  </button>
+                </>
+              ) : (
+                <>
+                  <Link
+                    to="/login"
+                    className="text-white hover:text-gray-300"
+                    onClick={handleItemClick}
+                  >
+                    Login
+                  </Link>
+                  <Link
+                    to="/register"
+                    className="ml-4 text-white hover:text-gray-300"
+                    onClick={handleItemClick}
+                  >
+                    Register
+                  </Link>
+                </>
+              )}
+            </div>
+          </div>
+          <div className="-mr-2 flex md:hidden ">
+            <button
+              onClick={() => {
+                setShowMenu((prevMenuState) => !prevMenuState);
+              }}
+              type="button"
+              ref={menuRef}
+              className="hamburger inline-flex items-center justify-center p-2 rounded-md text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
+              aria-controls="mobile-menu"
+              aria-expanded={showMenu ? "true" : "false"}
+            >
+              <span className="sr-only">Open main menu</span>
+              <FontAwesomeIcon icon={faBars} />
+            </button>
+          </div>
+        </div>
+      </div>
+
+      <div
+        className={`${showMenu ? "block" : "hidden"} md:hidden`}
+        id="mobile-menu"
+        ref={hamburgerRef}
+      >
+        <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3 text-right">
+          <Link
+            to="/"
+            className="text-white block hover:text-gray-300"
+            onClick={handleItemClick}
+          >
+            Home
+          </Link>
+          <Link
+            to="/filereader"
+            className="text-white block hover:text-gray-300"
+            onClick={handleItemClick}
+          >
+            FileReader
+          </Link>
+          {isLoggedIn && (
+            <Link
+              to="/savedfiles"
+              className="text-white block hover:text-gray-300"
+              onClick={handleItemClick}
+            >
+              Saved Files
+            </Link>
+          )}
+        </div>
+        <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3  flex justify-end text-right">
+          {isLoggedIn ? (
+            <div className="flex justify-end flex-col text-right">
+              <span className="text-white">Welcome {user.username}</span>
+              <button
+                onClick={handleLogout}
+                className="block mt-1 text-white hover:text-gray-300 text-right"
+              >
+                Logout <FontAwesomeIcon icon={faSignOutAlt} />
+              </button>
+            </div>
+          ) : (
+            <div className="flex justify-end flex-col text-right">
+              <Link
+                to="/login"
+                className="text-white block hover:text-gray-300 text-right"
+                onClick={handleItemClick}
+              >
+                Login
+              </Link>
+              <Link
+                to="/register"
+                className="mt-1 text-white block hover:text-gray-300 text-right"
+                onClick={handleItemClick}
+              >
+                Register
+              </Link>
+            </div>
+          )}
+        </div>
       </div>
     </nav>
   );
--- client-frontend/src/components/TableXLXS/FileInput.jsx
+++ client-frontend/src/components/TableXLXS/FileInput.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function FileInputComponent({ handleFileChange }) {
   return (
-    <div className="mb-2 flex items-center ">
+    <div className="mb-2 flex items-center justify-center">
       <input
         type="file"
         className="p-2 border-gray-300 border bg-white rounded mr-2"
