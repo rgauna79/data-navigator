239f56c16f10caae23bba2ca9945e887604b742c
RobertG <49545278+rgauna79@users.noreply.github.com>
2024-10-12 01:26:32 +0000

    update using hook useExcelData

--- client-frontend/src/components/TableXLXS/FileInput.jsx
+++ client-frontend/src/components/TableXLXS/FileInput.jsx
@@ -4,54 +4,10 @@ import { useDataContext } from "../../context/DataContext.jsx";
 import * as XLSX from "xlsx";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSpinner } from "@fortawesome/free-solid-svg-icons";
+import { useExcelData } from "../../hooks/useExcelData.js";
 
 function FileInput() {
-  const [isLoading, setIsLoading] = useState(false);
-  const { setFileData, setWorkbook, setSelectedSheet, workbook } =
-    useDataContext();
-
-  const formatJson = (json) => {
-    return json.map((row) =>
-      row.map((cell) =>
-        typeof cell === "string" ? cell.trim().toUpperCase() : cell
-      )
-    );
-  };
-
-  const handleFileChange = (e) => {
-    e.preventDefault();
-    setIsLoading(true);
-
-    const file = e.target.files[0];
-    if (!file) {
-      setIsLoading(false);
-      return;
-    }
-    if (!file.name.endsWith(".xlsx")) {
-      alert("Please select an xlsx file");
-      setIsLoading(false);
-      return;
-    }
-    const reader = new FileReader();
-
-    reader.onload = async (e) => {
-      try {
-        const wb = XLSX.read(e.target.result, { type: "binary" });
-        setWorkbook(wb);
-        const sheetNames = wb.SheetNames;
-        setSelectedSheet(sheetNames[0]);
-        const sheet = wb.Sheets[sheetNames[0]];
-        const json = XLSX.utils.sheet_to_json(sheet, { header: 1, raw: false });
-
-        setFileData(formatJson(json));
-      } catch (error) {
-        console.error("Error while reading file:", error);
-      }
-      setIsLoading(false);
-    };
-    reader.readAsBinaryString(file);
-  };
-
+  const { handleFileChange, isLoading } = useExcelData();
   return (
     <>
       <div className="mb-2 flex items-center justify-center">
@@ -61,7 +17,7 @@ function FileInput() {
           id="fileInput"
           aria-describedby="readFile"
           accept=".xlsx, application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
-          onChange={handleFileChange}
+          onChange={(e) => handleFileChange(e.target.files[0])}
         />
       </div>
       {isLoading && (
